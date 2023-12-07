7caf0c9e7dc3b66c7b6722d20c05fa9ffc82a32a
Toby Wilson <tobster86@gmail.com>
2023-12-07 21:13:29 +0000

    Battery gauge improvements

--- client/widgets/BatteryWidget.c
+++ client/widgets/BatteryWidget.c
@@ -1,14 +1,30 @@
 
 #include "BatteryWidget.h"
+#include <SDL2/SDL_image.h>
 
-#define NUB_OFFSET      0.1f
-#define NUB_WIDTHFRACT  3.0f
-#define AREA_OFFSET_PX  10
+#define BATT_TOP_OFFSET     0.15f
+#define BATT_HEIGHT_OFFSET  0.75f
+#define BATT_WIDTH_OFFSET   0.3f
 
 const SDL_Colour colBatt = { 100, 100, 100, 255 };
-const SDL_Colour col50 = { 0, 100, 0, 255 };
-const SDL_Colour col25 = { 100, 100, 0, 255 };
-const SDL_Colour colLow = { 100, 0, 0, 255 };
+const SDL_Colour col50 = { 0, 150, 0, 255 };
+const SDL_Colour col25 = { 150, 150, 0, 255 };
+const SDL_Colour colLow = { 150, 0, 0, 255 };
+
+SDL_Texture* textureBattery = NULL;
+
+static void BatteryWidget_InitAssets(SDL_Renderer* pRenderer)
+{
+    SDL_Surface* surface = IMG_Load("assets/batt.png");
+    
+    if(NULL != surface)
+    {
+        textureBattery = SDL_CreateTextureFromSurface(pRenderer, surface);
+        SDL_FreeSurface(surface);
+    }
+    else
+        printf("Failed to load battery texture.\n");
+}
 
 void BatteryWidget_Initialise(struct sdfWidget* psdcWidget,
                               float fltXOffset,
@@ -20,40 +36,33 @@ void BatteryWidget_Initialise(struct sdfWidget* psdcWidget,
     psdcWidget->psdcBatteryWidget = malloc(sizeof(struct sdfBatteryWidget));
     psdcWidget->psdcBatteryWidget->plBatteryPercent = plBatteryPercent;
     Widget_Initialise(psdcWidget, fltXOffset, fltYOffset, fltWidth, fltHeight, BatteryWidget_Update, BatteryWidget_ScreenChanged);
-        
 }
 
 void BatteryWidget_Update(struct sdfWidget* psdcWidget, SDL_Renderer* pRenderer)
 {
+    //Initialise assets if needed.
+    if(NULL == textureBattery)
+    {
+        BatteryWidget_InitAssets(pRenderer);
+    }
+
     //Draw battery body.
     SDL_SetRenderDrawColor(pRenderer, colBatt.r, colBatt.g, colBatt.b, colBatt.a);
-    SDL_Rect rectBody = { psdcWidget->lXPos,
-                          psdcWidget->lYPos + (((float)psdcWidget->lHeight) * NUB_OFFSET),
+    
+    SDL_Rect rectBckg = { psdcWidget->lXPos,
+                          psdcWidget->lYPos,
                           psdcWidget->lWidth,
-                          (((float)psdcWidget->lHeight) * (1.0f - NUB_OFFSET)) };
-    SDL_RenderFillRect(pRenderer, &rectBody);
-    
-    //Draw battery nub.
-    SDL_Rect rectNub = { psdcWidget->lXPos + (((float)psdcWidget->lWidth) / NUB_WIDTHFRACT),
-                         psdcWidget->lYPos,
-                         (((float)psdcWidget->lWidth) / NUB_WIDTHFRACT),
-                         (((float)psdcWidget->lHeight) * NUB_OFFSET) };
-    SDL_RenderFillRect(pRenderer, &rectNub);
-    
-    //Draw capacity area.
-    uint32_t lAreaHeight = (((float)psdcWidget->lHeight) * (1.0f - NUB_OFFSET)) - 2*AREA_OFFSET_PX;
-    SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
-    SDL_Rect rectArea = { psdcWidget->lXPos + AREA_OFFSET_PX,
-                          psdcWidget->lYPos + (((float)psdcWidget->lHeight) * NUB_OFFSET) + AREA_OFFSET_PX,
-                          psdcWidget->lWidth - 2*AREA_OFFSET_PX,
-                          lAreaHeight };
-    SDL_RenderFillRect(pRenderer, &rectArea);
-    
-    /*printf("Address of Widget: %p\n", (void*)psdcWidget);
-    printf("Address of Battery Widget: %p\n", (void*)psdcWidget->psdcBatteryWidget);
-    printf("The battery percent is %d", *psdcWidget->psdcBatteryWidget->plBatteryPercent);*/
+                          psdcWidget->lHeight };
+                          
+    SDL_RenderCopy(pRenderer, textureBattery, NULL, &rectBckg);
     
     //Draw capacity bar.
+    uint32_t lAreaTopOffset = (((float)psdcWidget->lHeight) * BATT_TOP_OFFSET + 0.5f);
+    uint32_t lAreaHeight = (((float)psdcWidget->lHeight * BATT_HEIGHT_OFFSET) + 0.5f);
+
+    uint32_t lAreaWidth = (((float)psdcWidget->lWidth) * (1.0f - BATT_WIDTH_OFFSET) + 0.5f);
+    uint32_t lAreaSideOffset = (psdcWidget->lWidth - lAreaWidth) / 2;
+    
     if(*psdcWidget->psdcBatteryWidget->plBatteryPercent >= 50)
         SDL_SetRenderDrawColor(pRenderer, col50.r, col50.g, col50.b, col50.a);
     else if(*psdcWidget->psdcBatteryWidget->plBatteryPercent >= 25)
@@ -63,9 +72,9 @@ void BatteryWidget_Update(struct sdfWidget* psdcWidget, SDL_Renderer* pRenderer)
         
     uint32_t lPowerHeight = (int)((((float)lAreaHeight / 100.0f) * (float)*psdcWidget->psdcBatteryWidget->plBatteryPercent) + 0.5f);
         
-    SDL_Rect rectBatt = { psdcWidget->lXPos + AREA_OFFSET_PX,
-                          psdcWidget->lYPos + (((float)psdcWidget->lHeight) * NUB_OFFSET) + AREA_OFFSET_PX + (lAreaHeight - lPowerHeight),
-                          psdcWidget->lWidth - 2*AREA_OFFSET_PX,
+    SDL_Rect rectBatt = { psdcWidget->lXPos + lAreaSideOffset,
+                          psdcWidget->lYPos + lAreaTopOffset + (lAreaHeight - lPowerHeight),
+                          lAreaWidth,
                           lPowerHeight };
     SDL_RenderFillRect(pRenderer, &rectBatt);
 }
